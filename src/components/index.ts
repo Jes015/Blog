7fc6d4d7614436a4f8367b533f3d0976f91b5960
Jes015 <jes.18.sin.15@gmail.com>
2023-09-02 05:13:17 +0000

    feat: re-design web site (#39)
    close #39

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,5 @@
 export * from './Categories/Categories'
 export * from './CommandMenu/CommandMenu'
-export * from './HeatMap/HeatMap'
 export * from './Post/Post'
 export * from './Search/Search'
 export * from './Tech/Tech'
