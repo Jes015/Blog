d551ea7fa73076f22de745996e5c877ca696df84
Jes015 <jes.18.sin.15@gmail.com>
2023-10-07 21:50:05 -0500

    feat: add social media links (#86)
    close #86

--- src/config/global.config.ts
+++ src/config/global.config.ts
@@ -1,34 +1,34 @@
 export const globalConfig = {
-    owner: {
-        siteName: 'Jes015\'s Blog',
-        name: 'Jesus Oyola',
-        nickName: 'Jes015',
-        socials: [
-            {
-                name: 'Portfolio',
-                url: 'https://portfolio-three-chi-27.vercel.app/'
-            },
-            {
-                name: 'Github',
-                url: 'https://github.com/Jes015'
-            },
-            {
-                name: 'Twitter',
-                url: "https://twitter.com/Jes_15_"
-            },
-        ]
-    },
-    localStorage: {
-        wasGlobalStepsShowed: 'wasGlobalStepsShowed'
-    },
-    recommendedSites: [
-        {
-            name: 'Dev Vault',
-            url: 'https://dev-vault-ochre.vercel.app/'
-        },
-        {
-            name: 'TimeManager',
-            url: 'https://time-manager-zeta.vercel.app/'
-        }
+  owner: {
+    siteName: 'Jes015\'s Blog',
+    name: 'Jesus Oyola',
+    nickName: 'Jes015',
+    socials: [
+      {
+        name: 'Portfolio',
+        url: 'https://portfolio-three-chi-27.vercel.app/'
+      },
+      {
+        name: 'Github',
+        url: 'https://github.com/Jes015'
+      },
+      {
+        name: 'Twitter',
+        url: 'https://twitter.com/Jes_15_'
+      }
     ]
+  },
+  localStorage: {
+    wasGlobalStepsShowed: 'wasGlobalStepsShowed'
+  },
+  recommendedSites: [
+    {
+      name: 'Dev Vault',
+      url: 'https://dev-vault-ochre.vercel.app/'
+    },
+    {
+      name: 'TimeManager',
+      url: 'https://time-manager-zeta.vercel.app/'
+    }
+  ]
 }
